5718d4679f5887f7475c3db4249c4201917f9541
Anton Mendelson <mendelson.inbox@gmail.com>
2024-01-09 10:57:17 +0200

    chore: add css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "@/app";
-
+import "@/app/styles/index.css";
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

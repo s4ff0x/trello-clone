15a46c2dfecdd8f6ffd383f976dd4f0150c7e72e
Anton Mendelson <mendelson.inbox@gmail.com>
2024-01-12 13:06:57 +0200

    feat: images

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import { ReactComponent as IconMail01 } from "./mail-01.svg";
+import { ReactComponent as IconAlertCircle } from "./alert-circle.svg";
+import { ReactComponent as IconArrowLeft } from "./arrow-left.svg";
+import { ReactComponent as Google } from "./google.svg";
+
+export { IconMail01, IconAlertCircle, IconArrowLeft, Google };
---
+++ src/shared/assets/images/index.ts
@@ -0,0 +1,4 @@
+import ImageLogomark from "./logomark.svg";
+import GeometricShapes from "./geometric-shapes.svg";
+
+export { ImageLogomark, GeometricShapes };
